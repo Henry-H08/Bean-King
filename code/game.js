da717e7cdd2ed15dc4bba0c6d8caeb38d76131d7
Henry-H08 <143623813+Henry-H08@users.noreply.github.com>
2024-07-30 20:35:13 -0400

    Update game.js

--- code/game.js
+++ code/game.js
@@ -1,30 +1,13 @@
 const SPEED = 320;
 
+var zoom = 0;
 
-function lines() {
-        add([
-            rect(12, 48),
-            area(),
-            pos(center()),
-            anchor("center"),
-            color(127, 127, 255),
-            outline(4),
-            move(DOWN, 320),
-            offscreen({ destroy: true }),
-            "line",
-        ]);
-    }
-
-onUpdate(() => {
-    wait(5, () => {
-    lines()
-})
-})
-
+var loopspeed = 1;
 
 const player = add([
     sprite("apple"),
     pos(center()),
+    z(5),
 ]);
 
 onKeyDown("left", () => {
@@ -35,6 +18,37 @@ onKeyDown("right", () => {
     player.move(SPEED, 0);
 });
 
+onKeyDown("up", () => {
+    zoom = zoom + 30;
+    loopspeed = loopspeed - 0.5;
+    debug.log(zoom);
+});
+
+onKeyDown("down", () => {
+    zoom = zoom - 30;
+    loopspeed = loopspeed + 0.5;
+    debug.log(zoom);
+});
+
+
+
+
+
+
+loop(loopspeed, () => {
+    add([
+            rect(12, 48),
+            area(),
+            pos(width()/2, (height()/15)-65),
+            anchor("top"),
+            color(127, 127, 255),
+            outline(4),
+            move(DOWN, 300),
+            offscreen({ destroy: true }),
+            z(1),
+            "line",
+        ]);
+})
 
 
 
